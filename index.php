28ed29799393c88947e28227a2248cbb5c6d109e
kenryanlukearias <pacifistkillua@gmail.com>
2024-05-30 07:49:52 +0400

    PROGRESS

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <img class="sag-image" src="SAGSHIPPING.jpg" alt="SAGSHIPPING">
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="quote(change to php).php">CONTACTS</a></li>
+        </ul>
+        <a class="quote-button" href="quote(change to php).php">QUOTE</a>
+    </nav>
+
+
+    <div class="main-body">
+        <img class="pic1" src="Images/pic1.png">
+        <div class="main-subbody">
+            <p class="main-title">SAGSHIPPING L.L.C.</p><br>
+            <p class="main-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis est et gravida tempus. Aliquam faucibus, orci id posuere sodales, orci nibh condimentum mauris, id ornare urna lacus at dolor.</p>
+        </div>
+    </div>
+
+    <div class="freights">
+        <div>
+            <img src="" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis est et gravida tempus. Aliquam faucibus, orci id posuere sodales, orci nibh condimentum mauris, id ornare urna lacus at dolor.</p>
+        </div>
+
+        <div>
+            <img src="" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis est et gravida tempus. Aliquam faucibus, orci id posuere sodales, orci nibh condimentum mauris, id ornare urna lacus at dolor.</p>
+        </div>
+
+        <div>
+            <img src="" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis est et gravida tempus. Aliquam faucibus, orci id posuere sodales, orci nibh condimentum mauris, id ornare urna lacus at dolor.</p>
+        </div>
+    </div>
+    
+</body>
+</html>
